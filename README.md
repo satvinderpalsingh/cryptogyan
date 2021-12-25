a03be2dec726573325026bf9abfbb322b8f71d3b
Satvinder Pal Singh <satvinderpalsingh52663@gmail.com>
2021-12-25 14:20:32 +0530

    dependency and setup

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-## A platform named CryptoGyan.
+# A platform named CryptoGyan :rocket:.
 ```
 - we can study and compare different crypto currenc💹💹💹💹.
 - Read about current news related to crypto📰🗞️📰🗞️📰.
 - Analyze crypto currencies using attractive charts📊📉📈💹. 
+```
+## Technichal Requirement packages/libraries for project :construction:.
+```
+- antd and ant-design/icons for styling and icons ⚡.
+- react-redux and reduxjs/toolkit for state mangement purpose and data flow in UI. 🚌.
+- axios for making api calls  📞.
+- chart.js and react-chartjs-2 helps to get the chart dor data representation and rendering on react aspplication 📊. 
+- moment to get the time fram 🕐.
+- millify to converts the large numbers to strings 🔢.
+- html-react-parser to The parser converts an HTML string to one or more React elements 🏪.
 ```
 
 
--- crypto-gyan/src/App.js
+++ crypto-gyan/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Switch,Link,Route} from 'react-router-dom'
+import {Typography,Layout,Space} from 'antd'
+import {Navbar} from './components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const app = () => {
+    return (
+        <div className='app'>
+            <div className='Navbar'>
+                <Navbar></Navbar>
+            </div>
+            <div className='main'>
+            </div>
+            <div className='Footer'>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default app
